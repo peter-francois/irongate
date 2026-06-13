a3abafa6879a5da8befba49c0a933f80ed2d7ebe
peter-francois <contact@peterfrancois.dev>
2026-06-13 10:31:26 +0200

    fix: change middlewares order

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import { logger } from "./middlewares/logger";
 import { auth } from "./middlewares/auth";
 
 const app = new Hono();
-app.use(auth)
 app.use(logger);
+app.use(auth)
 app.route("/metrics", metrics);
 
 export default app;

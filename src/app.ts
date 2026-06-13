06a1cf8a3435c8b70235ab162763d2ad76c121df
peter-francois <contact@peterfrancois.dev>
2026-06-13 10:29:51 +0200

    feat: add auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import metrics from "./routes/metrics";
 import { logger } from "./middlewares/logger";
+import { auth } from "./middlewares/auth";
 
 const app = new Hono();
+app.use(auth)
 app.use(logger);
 app.route("/metrics", metrics);
 
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,9 @@
+import type { MiddlewareHandler } from "hono";
+
+export const auth: MiddlewareHandler = async (c, next) => {
+  const token = c.req.header("Authorization");
+  if (token === `Bearer ${Bun.env.API_TOKEN}`) await next();
+  else {
+    return c.json({ error: "Unauthorized" }, 401)
+  }
+};

7c44d8f8ce46bb101064afc8931e4f44b67533fe
peter-francois <contact@peterfrancois.dev>
2026-06-13 08:54:34 +0200

    feat: create addMetric function

---
+++ src/store/metrics.ts
@@ -0,0 +1,13 @@
+import type { Metric } from "../types/metric";
+
+const store = new Map<string, Metric[]>();
+
+function addMetric(machineId: string, metric: Metric): void {
+  const existing = store.get(machineId);
+  
+  if (existing) {
+    existing.push(metric);
+  } else {
+    store.set(machineId, [metric]);
+  }
+}

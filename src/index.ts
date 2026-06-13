ab5b174ad249147c71181f7a0832c3e5fb765bae
peter-francois <contact@peterfrancois.dev>
2026-06-13 08:19:57 +0200

    chore: separate app definition from server entry point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { Hono } from 'hono'
+import app from "./app";
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+Bun.serve({
+  fetch: app.fetch,
+});
